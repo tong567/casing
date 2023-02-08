443c3e5c8e71a95947b88fddcb401ad26324fd9b
unknown <songsokvibol@gmail.com>
2023-02-08 09:06:11 +0700

    Project added

---
+++ camsing/src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+
+    
+
+ <>
+ <body>
+ <nav>
+ <img src="viboltong.png" alt="BigCo Inc. logo"width="40"/>
+      <ul>
+        <li>HOME</li>
+        <li>PRODUCT</li>
+        <li>ABOUT</li>
+        <li>CONTENT</li>
+      </ul>
+    </nav>
+    <div>
+    <img src="rrssr.png" alt="BigCo Inc. logo"/>
+      </div> 
+      <div>
+        <h1>hero</h1>
+      </div>
+      </body>
+
+
+    </>
+ 
+  );
+}
+
+export default App;
